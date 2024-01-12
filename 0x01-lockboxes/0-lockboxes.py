7bb0cd54e3df27375532f30fe51470fab8c08b75
Hilly-man <hilkiahe@gmail.com>
2024-01-12 00:22:40 +0000

    Lockerboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,11 +8,19 @@ def canUnlockAll(boxes):
         make a set of keys
             proceed to box 0
 
-                Obtain each key and add it. setofkeys: begin opening boxes using setofkeys
-                Proceed to every key box, extract the keys, and add them to the collection of keys.
+                Obtain each key and add it.
+                    setofkeys: begin opening boxes using setofkeys
+                Proceed to every key box, extract the keys,
+                    and add them to the collection of keys.
                 Continue going over each set of keys.
-            Disregard keys without a box. Track the number of boxes opened using a counter. If the total number of boxes at the end equals the number of boxes, then all of the boxes are unlocked.
-            OPTIMIZE IDEA: If setofkeys is increased by 0 in the beginning, there is no need for in 23
+            Disregard keys without a box. 
+                Track the number of boxes opened using a counter.
+                If the total number of boxes at the end
+                equals the number of boxes,
+                then all of the boxes are unlocked.
+            OPTIMIZE IDEA:
+            If setofkeys is increased by 0 in the beginning,
+            there is no need for in 23
 
     """
     total_boxes = len(boxes)
